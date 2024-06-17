a1d500ea6a5333580cdd3c1387949727fb0a8c83
Mark <ixjsx@icloud.com>
2024-06-17 20:28:34 +0300

    feat: add toolbar

--- Messenger/Messenger/Views/MainView.swift
+++ Messenger/Messenger/Views/MainView.swift
@@ -16,18 +16,63 @@ struct MainView: View {
                 .tabItem {
                     Image(systemName: "person.2.fill")
                 }
+                .tag(Tab.contacts)
             
             ChatsView()
                 .tabItem {
                     Image(systemName: "message.fill")
                 }
+                .tag(Tab.chats)
             
             MoreView()
                 .tabItem {
                     Image(systemName: "ellipsis.circle.fill")
                 }
+                .tag(Tab.more)
+        }
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                Text(titleForSelectedTab(selectedTab))
+            }
+            
+            ToolbarItem(placement: .topBarTrailing) {
+                HStack {
+                    switch selectedTab {
+                    case .contacts:
+                        Button(action: {
+                            print("Добавить контакт")
+                        }) {
+                            Image(systemName: "plus")
+                        }
+                    case .chats:
+                        Button(action: {
+                            print("Создать чат")
+                        }) {
+                            Image(systemName: "plus")
+                        }
+                        Button(action: {
+                            print("Прочитать все сообщения")
+                        }) {
+                            Image(systemName: "envelope.open.fill")
+                        }
+                    default:
+                        EmptyView()
+                    }
+                }
+            }
         }
         .navigationBarBackButtonHidden(true)
+    }
+    
+    private func titleForSelectedTab(_ tab: Tab) -> String {
+        switch tab {
+        case .contacts:
+            return "Контакты"
+        case .chats:
+            return "Чаты"
+        case .more:
+            return "Еще"
+        }
     }
 }
 
